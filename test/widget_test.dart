95369de1252235c80f5ea17a96acb6ba7c54ae72
BhetwalAyush <rujanbhetwal@gmail.com>
2025-08-09 12:41:34 +0545

    widget

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:soulpaws/main.dart';
+import 'package:furryfriends/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
